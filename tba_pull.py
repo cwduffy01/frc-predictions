aedd1e4a4ef3fb1b08dd7d1ce4f12d0a893121e9
Carson Duffy <31222907+cwduffy01@users.noreply.github.com>
2020-07-21 00:49:20 -0400

    Fixes auth_key error, adds get_event_info()

--- tba_pull.py
+++ tba_pull.py
@@ -4,6 +4,12 @@ import requests
 URL = "http://www.thebluealliance.com/api/v3"
 
 # authorization key used of API access
-AUTH_HEADERS = {}
-with open("X-TBA-Auth-Key.txt", 'r') as f:
-    AUTH_HEADERS["X-TBA-Auth-Key"] = f
+key = open("X-TBA-Auth-Key.txt", 'r').read()
+AUTH_HEADERS = {
+    'X-TBA-Auth-Key': key
+}
+
+
+def get_event_info(event_key):
+    events = requests.get(f"{URL}/event/{event_key}", headers=AUTH_HEADERS).json()
+    return events
