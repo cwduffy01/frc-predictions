d3ed420ce124c546a44325a2bc38cf3185d63327
Carson Duffy <31222907+cwduffy01@users.noreply.github.com>
2020-07-22 19:48:57 -0400

    Pulls team keys from rankings
    This way, teams that don't show up or don't play any matches aren't included

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-from tba_pull import get_matches, get_teams
+from tba_pull import get_matches, get_rankings
 
 
 def get_matrix(event_key):  # returns a coordinate matrix that displays who played which teams
 
     matches = get_matches(event_key)
 
-    teams = get_teams(event_key)
+    rankings = get_rankings(event_key)["rankings"]
     team_keys = []
-    for team in teams:
-        team_keys.append(team["key"])
+    for ranking in rankings:
+        team_keys.append(ranking["team_key"][3:])   # adds all team numbers to the team_keys list
+    team_keys.sort(key=int)                         # sorts team numbers numerically
+    while not team_keys[0].startswith("frc"):
+        element = f"frc{team_keys.pop(0)}"          # adds the "frc" back into each number to make it a key
+        team_keys.append(element)                   # appends it back into list
 
     coefficients = []
-    for i in range(len(team_keys)):
+    for i in range(len(team_keys)):     # creates blank matrix
         row = []
         for j in range(len(team_keys)):
             row.append(0)
         coefficients.append(row)
 
-    for match in matches:
-        if match["comp_level"] == "qm":
+    for match in matches:                                   # fill in matrix
+        if match["comp_level"] == "qm":                     # ignore bracket matches
             for alliance in match["alliances"].values():
                 for i in alliance["team_keys"]:
                     row = coefficients[team_keys.index(i)]
                     for j in alliance["team_keys"]:
                         row[team_keys.index(j)] += 1
+
+    return coefficients
+
+get_matrix("2020gadal")
