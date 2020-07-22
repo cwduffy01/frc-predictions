6849ceebc1ae3a038630b16165bda5f842fbfe14
Carson Duffy <31222907+cwduffy01@users.noreply.github.com>
2020-07-22 00:06:18 -0400

    Adds get_teams() function

--- tba_pull.py
+++ tba_pull.py
@@ -10,9 +10,13 @@ AUTH_HEADERS = {
 }
 
 
-def get_event_info(event_key):  # gets information about an event based on its key
+def get_event(event_key):  # gets information about an event based on its key
     return requests.get(f"{URL}/event/{event_key}", headers=AUTH_HEADERS).json()
 
 
 def get_matches(event_key):     # gets a list of information about matches at an event based on its key
     return requests.get(f"{URL}/event/{event_key}/matches", headers=AUTH_HEADERS).json()
+
+
+def get_teams(event_key):
+    return requests.get(f"{URL}/event/{event_key}/teams", headers=AUTH_HEADERS).json()
