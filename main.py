ea2ca11402bc20c9a0ec626cbcd55e0e100dc3db
Carson Duffy <31222907+cwduffy01@users.noreply.github.com>
2020-07-24 09:55:29 -0400

    Updates made to file save directory

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from predict import predict
 import pandas as pd
 import os
+
+from predict import predict
+from tba_pull import get_events
 
 
 def predict_event(event_key):
 
-    path = f"{event_key[:4]}_predictions"
+    path = f"event_predictions/{event_key[:4]}"
 
     try:
-        df = pd.read_csv(f"{path}/{event_key}_predictions.csv")
+        df = pd.read_csv(f"{path}/{event_key}.csv")
     except FileNotFoundError:                                   # if csv file doesn't exist
         df = predict(event_key)                                 # predict event
         try:
-            df.to_csv(f"{path}/{event_key}_predictions.csv")
+            df.to_csv(f"{path}/{event_key}.csv")
         except FileNotFoundError:                               # if directory for event year doesn't exist
-            os.mkdir(path)                                      # create directory
-            df.to_csv(f"{path}/{event_key}_predictions.csv")    # create csv file
+            os.makedirs(path)                                      # create directory
+            df.to_csv(f"{path}/{event_key}.csv")    # create csv file
 
     return df
