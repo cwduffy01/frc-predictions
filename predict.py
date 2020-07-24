7cb42e287889644526888c5483a86708dffcd029
Carson Duffy <31222907+cwduffy01@users.noreply.github.com>
2020-07-24 14:48:25 -0400

    teamKeys is the index of the returned DataFrame

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ from setup import get_matrix
 import numpy as np
 import pandas as pd
 
+
 def predict(event_key):
 
     data = get_matrix(event_key)
@@ -13,6 +14,13 @@ def predict(event_key):
     for category in categories:     # solve matrix for each category and store solutions
         b = []
         [b.append(row[category]) for row in values]
-        solutions[category] = list(np.linalg.solve(np.array(a), np.array(b)))
+        if type(b[0]) == int:   # metric is alliance-based
+            solutions[category] = list(np.linalg.solve(np.array(a), np.array(b)))   # solve matrix
+        else:                   # metric is team-based
+            solutions[category] = []
+            [solutions[category].append(value/a[0][0]) for value in b]  # take average for each team
 
-    return pd.DataFrame(solutions, index=data[2])   # return pandas DataFrame of predicted data
+    df = pd.DataFrame(solutions)                        # return pandas DataFrame of predicted data
+    df["teamKey"] = pd.Series(data[2])
+    df.set_index("teamKey", inplace=True, drop=True)    # makes teamKeys the index
+    return df
