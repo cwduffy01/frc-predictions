f483786869aa50efaf6d9b4f8f47c0a90c378ea4
Carson Duffy <31222907+cwduffy01@users.noreply.github.com>
2020-07-31 21:19:52 -0400

    Accounts for teams that have not competed
    Also includes slight modifications to the path at which files are saved

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from predict import predict
 from tba_pull import get_events, get_rankings, get_teams
 
 
-def get_event_predictions(event_key):
+def get_event_predictions(event_key, overwrite=False):
 
-    path = f"event_predictions/{event_key[:4]}"
+    path = f"predictions/event/{event_key[:4]}"
     filename = event_key
 
     try:    # try opening csv file with predictions
+        if overwrite:   # raise error if csv file needs to be rewritten
+            raise FileNotFoundError
         df = pd.read_csv(f"{path}/{filename}.csv")
         df.set_index("teamKey", inplace=True, drop=True)
 
     except FileNotFoundError:       # if predictions have not been made yet
+
         df = predict(event_key)     # predict event
 
         try:    # try writing csv to path
@@ -29,16 +32,18 @@ def get_event_predictions(event_key):
     return df
 
 
-def get_team_predictions(team_key, *years, offseason=False):
+def get_team_predictions(team_key, *years, offseason=False, overwrite=False):
 
     years = list(years)     # convert tuple to list
     years.sort()
     years = [str(year) for year in years]
 
-    path = f"team_predictions/{team_key}"
+    path = f"predictions/team/{team_key}"
     filename = f"{'_'.join(years)}{'_os' if offseason else ''}"
 
     try:    # try opening csv file with predictions
+        if overwrite:   # raise error if csv file needs to be rewritten
+            raise FileNotFoundError
         df = pd.read_csv(f"{path}/{filename}.csv")
         df.set_index("eventKey", inplace=True, drop=True)
 
@@ -55,7 +60,7 @@ def get_team_predictions(team_key, *years, offseason=False):
             if event["event_type"] != 4:    # doesn't count einstein
                 if event["event_type"] not in [99, 100] or offseason:       # count offseason events if specified
                     if len(get_rankings(event["key"])["rankings"]) != 0:    # if event has happened already
-                        event_df = get_event_predictions(event["key"]).loc[[team_key]]   # isolate single row for team
+                        event_df = get_event_predictions(event["key"], overwrite).loc[[team_key]]   # isolate single row for team
                         event_df["eventKey"] = pd.Series([event["key"]], index=event_df.index)  # add eventKey series
                         event_df.set_index("eventKey", inplace=True, drop=True)         # change index to eventKey
                         event_dfs.append(event_df)
@@ -71,30 +76,57 @@ def get_team_predictions(team_key, *years, offseason=False):
     return df
 
 
-def get_future_event_predictions(future_event_key):     # returns best scores for each team at an upcoming event
+def get_future_event_predictions(future_event_key, overwrite=False):    # returns best scores for each team at an upcoming event
 
-    team_keys = get_teams(future_event_key, keys=True)          # get team keys and sort
-    team_keys = sorted(team_keys, key=lambda i: int(i[3:]))
-    dfs = []
+    if len(get_rankings(future_event_key)["rankings"]) != 0:    # if event has already occurred
+        print("Event has already occurred")
+        return get_event_predictions(future_event_key, overwrite)
 
-    for team_key in team_keys:
-        events = get_events(team_key, year=future_event_key[:4])    # get list of events
-        events = sorted(events, key=lambda i: i["start_date"])      # sort by start date
-        event_keys = [event["key"] for event in events]             # get list of event keys
-        del event_keys[event_keys.index(future_event_key):]         # slice list to events before predicted event
+    path = f"predictions/future_event/{future_event_key[:4]}"
+    filename = future_event_key
 
-        event_dfs = []
-        for event_key in event_keys:
-            event_dfs.append(get_event_predictions(event_key).loc[[team_key]])  # get each event row for team
-        event_df = pd.concat(event_dfs)     # combine
+    try:                # try opening csv file with predictions
+        if overwrite:   # raise error if csv file needs to be rewritten
+            raise FileNotFoundError
+        df = pd.read_csv(f"{path}/{filename}.csv")
+        df.set_index("teamKey", inplace=True, drop=True)
 
-        min_df = event_df[["foulCount", "techFoulCount", "totalFoulCount"]].min().to_frame().transpose()    # min fouls
-        event_df = event_df.max().to_frame().transpose()    # get max scores over all events
-        event_df.update(min_df)                             # modify fouls columns for minimum fouls
-        event_df["teamKey"] = team_key
-        event_df.set_index("teamKey", inplace=True)         # change index to team key
-        dfs.append(event_df)
+    except FileNotFoundError:
 
-    df = pd.concat(dfs)     # merge all dataframes
+        team_keys = get_teams(future_event_key, keys=True)          # get team keys and sort
+        team_keys = sorted(team_keys, key=lambda i: int(i[3:]))
+        dfs = []
+
+        for team_key in team_keys:
+
+            events = get_events(team_key, year=future_event_key[:4])    # get list of events
+            events = sorted(events, key=lambda i: i["start_date"])      # sort by start date
+            event_keys = [event["key"] for event in events]             # get list of event keys
+            del event_keys[event_keys.index(future_event_key):]         # slice list to events before predicted event
+
+            event_dfs = []
+            for event_key in event_keys:
+                if team_key in [ranking["team_key"] for ranking in get_rankings(event_key)["rankings"]]:    # if team competed
+                    event_dfs.append(get_event_predictions(event_key, overwrite).loc[[team_key]])  # get each event row
+
+            if len(event_dfs) != 0:
+                event_df = pd.concat(event_dfs)     # combine
+                min_df = event_df[["foulCount", "techFoulCount", "totalFoulCount"]].min().to_frame().transpose()
+                event_df = event_df.max().to_frame().transpose()    # get max scores over all events
+                event_df.update(min_df)                             # modify fouls columns for minimum fouls
+                event_df["teamKey"] = team_key
+                event_df.set_index("teamKey", inplace=True)         # change index to team key
+                dfs.append(event_df)
+
+        df = pd.concat(dfs)     # merge all dataframes
+
+        try:  # try writing csv to path
+            df.to_csv(f"{path}/{filename}.csv")
+
+        except FileNotFoundError:                # if path doesn't exist
+            os.makedirs(path)                    # create directory
+            df.to_csv(f"{path}/{filename}.csv")  # create csv file
 
     return df
+
+get_future_event_predictions("2020gacar")
